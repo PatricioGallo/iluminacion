a1e3f3566057e9ee04f563e88485ff6a0400aff4
PatricioGallo <patogallo95@gmail.com>
2022-08-11 20:29:33 -0300

    web completa

---
+++ js/main.js
@@ -0,0 +1,57 @@
+
+
+// FUNCIONES
+
+function funcionclick(id){
+  let item = workList.find((lista) => lista.id === id);
+
+  informacion_works.innerHTML=`
+  <div class="cuerpo_informacion">
+    <div class="fondo_informacion">
+
+      <div class="cruz">
+        <button type="button" name="button" onclick="cerrarTF()">X</button>
+      </div>
+
+      <img src="media/imagenes/${item.imagen}" alt="">
+
+      <div class="contenido_informacion">
+        <h1>${item.nombre}</h1>
+        <p>${item.p1}</p>
+        <p>${item.p2}</p>
+        <p>${item.p3}</p>
+        <p>${item.p4}</p>
+        <div class="botones_informacion">
+            <a href="" target="_blank"> <button type="button" name="button">IR A TIENDA</button> </a>
+        </div>
+      </div>
+
+    </div>
+  </div>
+  `
+}
+
+
+function cerrarTF(){
+  informacion_works.innerHTML=""
+}
+
+
+
+
+//PROGRAMA
+
+let cuerpo_productos = document.getElementById("cuerpo_productos")
+
+
+for(const lista of workList){
+  let productos_contenido = document.createElement("div");
+  productos_contenido.innerHTML= `
+  <div class="contenedor_producto" onclick="funcionclick(${lista.id})">
+
+      <img src="media/imagenes/${lista.imagen}" alt="">
+
+  </div>
+  `
+  cuerpo_productos.append(productos_contenido)
+}
---
+++ js/works.js
@@ -0,0 +1,78 @@
+const workList = [{
+  id:1,
+  nombre:"Lampara Atenas",
+  tipo:"Lampara colgante",
+  precio:"4500",
+  cuotas:"",
+  imagen:"atenas.png",
+  p1:"Este diseño es un fuego",
+  p2:"Diseñado con el espesor justo para generar una iluminación homogénea en todo el ambiente aportándole vida y estilo.",
+  p3:"",
+  p4:"",
+  medidas: "30x30x30 cm"
+},
+{
+  id:2,
+  nombre:"Lampara Grecia",
+  tipo:"Lampara velador",
+  precio:"3000",
+  cuotas:"",
+  imagen:"",
+  p1:"Presentamos nuestro nuevo modelo velador Grecia",
+  p2:"Como todas nuestras lámparas, el velador Grecia es fabricado con un biopolímero amigable con el medio ambiente ",
+  p3:"La luz transparenta el material por lo que crea un efecto difuso dándole un toque de calidez y relax a tus ambientes.",
+  p4:"Especiales para usar en mesa de luz o escritorio. El modelo de la foto es en color rosa pastel a pedido especial. Consultar por colores disponibles.",
+  medidas:"20 x 20 x 20 cm"
+},
+{
+  id:3,
+  nombre:"Lampara Roma",
+  tipo:"Lampara colgante",
+  precio:"3700",
+  cuotas:"",
+  imagen:"roma.jpeg",
+  p1:"Lampara lista para su instalación",
+  p2:"Incluye: •Florón (Tapa del techo) •Cable de 75cm de largo •Prensa cables •Portalámparas",
+  p3:"",
+  p4:"",
+  medias:"30 x 27 cm"
+},
+{
+  id:4,
+  nombre:"Lampara Calabaza",
+  tipo:"Lampara colgante",
+  precio:"3700",
+  cuotas:"",
+  imagen:"calabaza.jpeg",
+  p1:"Características: Material PLA",
+  p2:"",
+  p3:"",
+  p4:"",
+  medidas:"30 cm de diámetro y 20 cm de alto"
+},
+{
+  id:5,
+  nombre:"Lampara Finlandia",
+  tipo:"Lampara colgante",
+  precio:"3700",
+  cuotas:"",
+  imagen:"finlandia.jpeg",
+  p1:"Este diseño es un fuego",
+  p2:"Diseñado con el espesor justo para generar una iluminación homogénea en todo el ambiente aportándole vida y estilo.",
+  p3:"",
+  p4:"",
+  medidas:"30 x 30 x 30 cm"
+},
+{
+  id:6,
+  nombre:"Velador Finlandia",
+  tipo:"velador",
+  precio:"3000",
+  cuotas:"",
+  imagen:"velador_finlandia.jpeg",
+  p1:"Este diseño es un fuego",
+  p2:"Diseñado con el espesor justo para generar una iluminación homogénea en todo el ambiente aportándole vida y estilo.",
+  p3:"",
+  p4:"",
+  medidas:"20 x 20 x 20 cm"
+}]
